b0ae39b6560ee2350b15c1449ec3d36f96cadd83
kwanred <obkeiji@gmail.com>
2025-08-01 18:22:26 +0900

    kadai_sortを再アップロード

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,29 +9,28 @@
 <body>
     <p>
         <?php
-     
+        $nums = [15, 4, 18, 23, 10 ];
         $order = '昇順';
 
-        function sort_2way($order) {
-          $nums = [15, 4, 18, 23, 10 ];
+        function sort_2way($array, $order) {
           if ($order === '昇順') {
             echo '昇順にソートします。<br>';
-            sort ($nums);
-            foreach ($nums as $array){
-              echo $array . '<br>';
+            sort ($array);
+            foreach ($array as $num){
+              echo $num . '<br>';
             }
           } else {
             echo '降順にソートします。<br>';
-            rsort ($nums);
-            foreach ($nums as $array){
-              echo $array . '<br>';
+            rsort ($array);
+            foreach ($array as $num){
+              echo $num . '<br>';
             }
           }
         }
 
-        sort_2way('昇順');
+        sort_2way($nums, '昇順');
 
-        sort_2way('降順');
+        sort_2way($nums, '降順');
 
         
         ?>
